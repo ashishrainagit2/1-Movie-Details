d30b1f08b5545f5b48fd35e45631856c27dccac8
ashishrainagit2 <ashish.raina@bookmyshow.com>
2019-08-13 17:49:19 +0530

    host as github page

---
+++ build/precache-manifest.211f3384015879404a5a8f1d71dc9c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54d18cb9906b0e997913a0b452b18eb",
+    "url": "/1-Movie-Details/index.html"
+  },
+  {
+    "revision": "424f6e7635f1deb60a2d",
+    "url": "/1-Movie-Details/static/css/main.6fdefba6.chunk.css"
+  },
+  {
+    "revision": "beabec758bfb4d7e60b3",
+    "url": "/1-Movie-Details/static/js/2.d4b7386b.chunk.js"
+  },
+  {
+    "revision": "424f6e7635f1deb60a2d",
+    "url": "/1-Movie-Details/static/js/main.9a930364.chunk.js"
+  },
+  {
+    "revision": "bb64e3c12dceac832af1",
+    "url": "/1-Movie-Details/static/js/runtime~main.05e8a413.js"
+  }
+]);
