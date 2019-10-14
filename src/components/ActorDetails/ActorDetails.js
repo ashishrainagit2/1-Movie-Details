ecf77dc5d587d8a31a2ec74b9ae64f0645e3a564
ashishrainagit2 <ashish.raina@bookmyshow.com>
2019-10-14 21:00:19 +0530

    added few more details

--- src/components/ActorDetails/ActorDetails.js
+++ src/components/ActorDetails/ActorDetails.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import classes from './ActorDetails.module.css';
 
 const actorDetails = (props) => {
 
-    
     if(props.details){
-        console.log(">>>>>>>>" , props.details.profile_path)
+        console.log(">>>>>>>>" , props.details)
         return  (
-            <div>
-                <span>
+            <div className={classes.ActorDetails}>
+                <div className={classes.media}>
                     <img src={"https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + props.details.profile_path} alt="actor"/>
-                </span>
-                <span>
+                </div>
+                <div className={classes.info}>
                     <h1>Name: {props.details.name}</h1>
                     <p>About: {props.details.biography}</p>
                     <p>Birthday: {props.details.birthday}</p>
                     <p>Known for : {props.details.known_for_department}</p>
-                    <p>Gender: {props.details.gender}</p>
+                    <p>Gender: {props.details.gender == 2 ? "Male" : "female"}</p>
                     <p>Place of birth : {props.details.place_of_birth}</p>
-                </span>
+                    <p>Popularity: {props.details.popularity}</p>
+                </div>
             </div>
         )
     }
--- src/components/TvshowPage/Tvshowpage.js
+++ src/components/TvshowPage/Tvshowpage.js
@@ -1,31 +1,33 @@
 import React from 'react';
+import classes from './Tvshowpage.module.css';
 
 const tvshowpage =  (props) => {
     console.log(props.details)
     return (
         <div>
-            <div>
-                <img src={"https://image.tmdb.org/t/p/w500/" + props.details.poster_path} alt=""/>
-            </div>
+            <div className={classes.Tvshowpage}>
+                <div className={classes.media}>
+                    <img src={"https://image.tmdb.org/t/p/w342/" + props.details.poster_path} alt=""/>
+                    {/* <button>Play Trailer</button> */}
+                </div>
 
-            <div>
-                Details:
-                <span>original_name :  {props.details.riginal_name}</span> <br />
-                <span>overview  : {props.details.overview}</span> <br />
-                <span> popularity : {props.details.popularity}</span> <br />
-                <span> number_of_seasons : {props.details.number_of_seasons}</span> <br />
-                <span> origin_country : {props.details.origin_country}</span> <br />
-                <span> status : {props.details.status}</span> <br />
-                <span> vote_average : {props.details.vote_average}</span> <br />
-                <span> vote_count : {props.details.vote_count}</span> <br />
-                <span> status : {props.details.status}</span> <br />
-                <span> last_air_date : {props.details.last_air_date}</span> <br />
-                <span> in_production : {props.details.in_production}</span> <br />
-                <span> homepage : {props.details.homepage}</span> <br />
-                <span> first_air_date : {props.details.first_air_date}</span> <br />
-                <span> episode_run_time : {props.details.episode_run_time}</span>
+                <div className={classes.info}>
+                    Details:<br />
+                    <span><strong>Name: </strong> {props.details.original_name}</span> <br />
+                    <span><strong>Overview  :</strong> {props.details.overview}</span> <br />
+                    <span><strong>Popularity :</strong>  {props.details.popularity}</span> <br />
+                    <span><strong>Seasons :</strong>  {props.details.number_of_seasons}</span> <br />
+                    <span><strong>Country :</strong>  {props.details.origin_country}</span> <br />
+                    <span><strong>Status : </strong> {props.details.status}</span> <br />
+                    <span><strong>Rating :</strong>  {props.details.vote_average}</span> <br />
+                    <span><strong>Vote Count :</strong>  {props.details.vote_count}</span> <br />
+                    <span><strong>Last Aired :</strong>  {props.details.last_air_date}</span> <br />
+                    <span><strong>Production Status :</strong>  {props.details.in_production}</span> <br />
+                    <span><strong>Homepage : </strong> {props.details.homepage}</span> <br />
+                    <span><strong>First Aired :</strong>  {props.details.first_air_date}</span> <br />
+                    <span><strong> Episode Duration : </strong>{props.details.episode_run_time}</span>
+                </div>
             </div>
-            
         </div>
     )
 }
